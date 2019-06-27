76602f57a48549eccf14fd565f2600d59ddc6f5a
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-06-27 08:24:42 -0400

    Added logic to add Artist to firebase

--- app/src/main/java/com/falcondevelopers/artistlist/MainActivity.java
+++ app/src/main/java/com/falcondevelopers/artistlist/MainActivity.java
@@ -9,16 +9,23 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText etArtistName;
     Button btAddArtist;
     Spinner spinnerGenres;
 
+    DatabaseReference dbArtists;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbArtists = FirebaseDatabase.getInstance().getReference("artists");
 
         etArtistName = (EditText) findViewById(R.id.etArtistName);
         btAddArtist = (Button) findViewById(R.id.btAddArtist);
@@ -37,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
          String genre = spinnerGenres.getSelectedItem().toString();
 
          if(!TextUtils.isEmpty(artistName)){
+
+             String id = dbArtists.push().getKey();
+
+             Artist artist = new Artist(id, artistName, genre);
+
+             dbArtists.child(id).setValue(artist);
+
+             Toast.makeText(this,"Artist added!!!",Toast.LENGTH_SHORT).show();
 
          }else{
              Toast.makeText(this, "Please enter artist name",Toast.LENGTH_SHORT).show();
